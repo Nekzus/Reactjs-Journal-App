e5349956a4b0135553d91ca344a2bfa1787c74ec
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-13 00:07:37 -0300

    Conexiones a Firestore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,14 +2,21 @@ import { IconButton } from '@mui/material';
 import { AddOutlined, ImportContacts } from '@mui/icons-material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { useDispatch, useSelector } from 'react-redux';
+import { startNewNote } from '../../store/journal/thunks';
 
 export const JournalPage = () => {
+    const { isSaving, active } = useSelector(state => state.journal);
+    const dispatch = useDispatch();
+    const onClickNewNote = () => {
+        dispatch(startNewNote());
+    };
     return (
         <JournalLayout>
-            <NothingSelectedView />
-            {/* <NoteView /> */}
-
+            {!!active ? <NoteView /> : <NothingSelectedView />}
             <IconButton
+                disabled={isSaving}
+                onClick={onClickNewNote}
                 size="large"
                 sx={{
                     color: 'white',
